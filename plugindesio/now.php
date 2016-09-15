3b422e326e8379a1f327c3d0fd7b6ee741c120e5
Maurizio De Magnis <root@olisti.co>
2016-09-15 23:48:52 +0200

    code makeup

--- plugindesio/now.php
+++ plugindesio/now.php
@@ -1,5 +1,5 @@
 <?php
-  require __DIR__ . '/../vendor/autoload.php';
+  require __DIR__."/../vendor/autoload.php";
 
   $dotenv = new Dotenv\Dotenv(__DIR__, "../_env");
   $dotenv->load();
@@ -8,10 +8,10 @@
   $meteo = file_get_contents("http://api.wunderground.com/api/${token}/conditions/lang:IT/q/Italy/Desio.json");
 
   $json = json_decode($meteo, true);
-  $condizione = $json['current_observation']['weather'];
-  $temp= $json['current_observation']['temp_c'];
-  $umidita=$json['current_observation']['relative_humidity'];
-  $icon=$json['current_observation']['icon'];
+  $condizione = $json["current_observation"]["weather"];
+  $temp = $json["current_observation"]["temp_c"];
+  $umidita = $json["current_observation"]["relative_humidity"];
+  $icon = $json["current_observation"]["icon"];
 
 
   $unicode_mapping = [
@@ -34,7 +34,7 @@
 
    $icont = preg_replace("/\\\\u([0-9a-fA-F]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", $icont);
 
-  echo 'Condizioni: '.$condizione." ".$icont."\r\n";
-  echo 'Temperatura: '.$temp."° C\r\n";
-  echo 'Umidità: '.$umidita."\r\n";
+  echo "Condizioni: ${condizione} ${icont}\r\n";
+  echo "Temperatura: ${temp}° C\r\n";
+  echo "Umidità: ${umidita}\r\n";
 ?>
